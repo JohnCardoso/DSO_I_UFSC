958245bd2e0de8188608abfe0195728bc129bf75
Marcelo Brosowicz de Paulo <mbrosowicz@hotmail.com>
2017-04-19 21:01:00 -0300

    Add files via upload

---
+++ KeySys.java
@@ -0,0 +1,13 @@
+package br.ufsc.ine5605.trab1;
+
+import br.ufsc.ine5605.trab1.controllers.ControladorFuncionario;
+
+public class KeySys {
+
+	public static void main(String[] args) {
+		ControladorFuncionario ctrlFuncionario = new ControladorFuncionario();
+		ctrlFuncionario.inicia();
+
+	}
+
+}
---
+++ interfaces/IRucd.java
@@ -0,0 +1,11 @@
+package br.ufsc.ine5605.trab1.interfaces;
+
+public interface IRucd {
+
+	public void cadastrar(Object o);
+
+	public void alterar(String verificador, Object o);
+
+	public void excluir(String verificador);
+
+}
