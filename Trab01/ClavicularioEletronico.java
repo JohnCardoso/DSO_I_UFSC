ebe5d78683657b3b882bbd9d30d1c10cf5d69f26
Marcelo Brosowicz de Paulo <mbrosowicz@hotmail.com>
2017-04-19 21:16:35 -0300

    Add files via upload

---
+++ Trab01/ClavicularioEletronico.java
@@ -0,0 +1,15 @@
+package br.ufsc.ine5605.trab1;
+
+import java.util.ArrayList;
+
+import br.ufsc.ine5605.trab1.objects.Veiculo;
+
+public class ClavicularioEletronico {
+
+	private ArrayList<Veiculo> listaChaves;
+
+	public ClavicularioEletronico() {
+		this.listaChaves = new ArrayList<>();
+	}
+
+}
---
+++ Trab01/display/TelaVeiculo.java
@@ -0,0 +1,16 @@
+package br.ufsc.ine5605.trab1.display;
+
+import br.ufsc.ine5605.trab1.controllers.ControladorVeiculo;
+
+public class TelaVeiculo {
+
+	public TelaVeiculo(ControladorVeiculo controladorVeiculo) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void exibeMenuInicial() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
