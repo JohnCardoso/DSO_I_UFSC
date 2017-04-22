2106ff944d520c813567ff037227541d8dc3e014
Jonathan <giam.cardoso@gmail.com>
2017-04-21 21:25:54 -0300

    Update ControladorVeiculo.java

--- Trab01/controllers/ControladorVeiculo.java
+++ Trab01/controllers/ControladorVeiculo.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import br.ufsc.ine5605.trab1.display.TelaVeiculo;
 import br.ufsc.ine5605.trab1.interfaces.IRucd;
 import br.ufsc.ine5605.trab1.objects.Veiculo;
+import br.ufsc.ine5605.trab1.objects.Message;
 
 public class ControladorVeiculo implements IRucd {
 
@@ -19,24 +20,38 @@ public class ControladorVeiculo implements IRucd {
 	}
 
 	public void inicia() {
-		telaVeiculo.exibeMenuInicial();
+		telaVeiculo.exibeMenuInicialVeiculo();
 	}
+        
+        public Veiculo buscarPelaPlaca(String placa) {
+		if (verificaVeiculoExiste(placa)) {
+			for (Veiculo v : listaVeiculos) {
+				if (v.getPlaca().equals(placa)) {
+					return v;
+				}
+			}
+		}
+		return null;
+        }
 
 	public ArrayList<Veiculo> listarVeiculos() {
 		return listaVeiculos;
 	}
 
 	public String exibeListaVeiculos(ArrayList<Veiculo> listaVeiculos) {
+                String listaDeVeic = "";
 		if (listaVeiculos != null) {
 			if (!listaVeiculos.isEmpty()) {
 				for (Veiculo veiculo : listaVeiculos) {
-					return ("\nDigitos da placa: " + veiculo.getPlaca() + "\nModelo: " + veiculo.getModelo()
+					listaDeVeic += ("\nPlaca: " + veiculo.getPlaca() + "\nModelo: " + veiculo.getModelo()
 							+ "\nMarca: " + veiculo.getMarca() + "\nAno: " + veiculo.getAno()
-							+ "\nQuilometragem Atual: " + veiculo.getQuilometragemAtual());
+							+ "\nQuilometragem Atual: " + veiculo.getQuilometragemAtual() + "\n");
 				}
 			}
-		}
-		return "Lista Vazia ou Nula";
+		} else {
+                    new Message("Lista Vazia ou Nula");
+                }
+                return listaDeVeic;
 	}
 
 	@Override
@@ -46,46 +61,33 @@ public class ControladorVeiculo implements IRucd {
 			if (!listaVeiculos.isEmpty()) {
 				if (!listaVeiculos.contains(veic)) {
 					listaVeiculos.add(veic);
-					System.out.println("Veiculo cadastrado com sucesso!\n");
-					exibeListaVeiculos(listaVeiculos);
+					new Message("Veiculo cadastrado com sucesso!\n");					
 				}
-			}
-			listaVeiculos.add(veic);
-			System.out.println("Veiculo cadastrado com sucesso!\n");
-			exibeListaVeiculos(listaVeiculos);
-		}
-		System.out.println("Erro ao cadastrar: Veiculo nulo");
+                        } else {
+                            listaVeiculos.add(veic);
+                            new Message("Veiculo cadastrado com sucesso!\n");
+                        }
+                } else {
+                    new Message("Erro ao cadastrar: Veiculo nulo");
+                }		
 	}
 
 	@Override
 	public void alterar(String placa, Object veiculo) {
 		if (veiculo != null) {
-			Veiculo veic = (Veiculo) veiculo;
-			if (codigo != 0) {
-				if (!listaVeiculos.isEmpty()) {
-					for (Veiculo veiculoAlterar : listaVeiculos) {
-						if (veiculoAlterar.getCodigo() == codigo) {
-							listaVeiculos.set(codigo - 1, veic);
-							break;
-						}
-					}
-				}
-			}
-		}
-		System.out.println("Erro ao alterar: Lista Vazia/Nula ou Veiculo Nulo");
+		} else {
+                    new Message("Erro ao alterar: Lista Vazia/Nula ou Veiculo inexistente");
+                }
+		
 	}
 
 	@Override
 	public void excluir(String placa) {
-		if (codigo != 0) {
-			if (!listaVeiculos.isEmpty()) {
-				for (Veiculo veiculoExcluir : listaVeiculos) {
-					if (veiculoExcluir.getCodigo() == codigo) {
-						listaVeiculos.remove(veiculoExcluir);
-						break;
-					}
-				}
-			}
+            if (verificaVeiculoExiste(placa)) {
+			listaVeiculos.remove(buscarPelaPlaca(placa));
+			new Message("Veiculo removido com sucesso!");
+		} else {
+			new Message("Erro ao remover: Lista Vazia/Nula ou placa não cadastrada!");
 		}
 	}
 
@@ -97,6 +99,37 @@ public class ControladorVeiculo implements IRucd {
 		}
 
 		return validade;
+	}
+        
+        public boolean verificaVeiculoExiste(String placa) {
+		if (!listaVeiculos.isEmpty()) {
+			for (Veiculo v : listaVeiculos) {
+				if (v.getPlaca().equals(placa)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+        
+        public boolean validadeAno(int ano) {
+		boolean validAno = false;
+
+		if (ano <= 2017 && ano >= 1990) {
+			validAno = true;
+		}
+
+		return validAno;
+	}
+        
+        public boolean validadeQuilometragem(int quilometragemAtual) {
+		boolean validKm= false;
+
+		if (quilometragemAtual >= 0 && quilometragemAtual <= 200000) {
+			validKm = true;
+		}
+
+		return validKm;
 	}
 
 }
